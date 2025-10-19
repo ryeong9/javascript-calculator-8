a698b9cade728fde37e86c4f8306b911a1f3e7a6
ryeong <oaoa0728@naver.com>
2025-10-19 16:54:17 +0900

    refactor(validator): 숫자 검증 로직을 강화하여 정수만 허용하도록 수정

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,13 +2,15 @@ import { ERROR_MESSAGES } from "../constants/errorMessages.js";
 
 export function validateAndConvert(values) {
   return values.map((value) => {
-    const num = Number(value);
+    const trimmed = value.trim();
+    const num = Number(trimmed);
 
-    if (isNaN(num)) {
-      throw new Error(ERROR_MESSAGES.INVALID_NUMBER);
-    }
     if (num < 0) {
       throw new Error(ERROR_MESSAGES.NEGATIVE_NUMBER);
+    }
+
+    if (!/^\d+$/.test(trimmed)) {
+      throw new Error(ERROR_MESSAGES.INVALID_NUMBER);
     }
 
     return num;
